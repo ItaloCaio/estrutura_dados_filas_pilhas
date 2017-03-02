8b8114449b83a7f51e1fa2b63749295d2f74975b
ItaloCaio <it-caio@hotmail.com>
2017-03-02 14:12:55 -0300

    Teminando a primeira parte

--- src/implementacao2/BrincandoComOrdenacao.java
+++ src/implementacao2/BrincandoComOrdenacao.java
@@ -5,19 +5,332 @@ import java.util.Random;
 public class BrincandoComOrdenacao {
 
 	public static void main(String[] args) {
-		// TODO Codifique o experimento pedido. Crie metodos auxiliares para
-		// facilitar.
 
-		int x[] = { 1, 8, 2, 5, 1 };
+		int arrayOrdenado[] = new int[20];
+		int arrayInversamenteOrd[] = new int[20];
+		int arrayAleatorio[] = new int[20];
+		Random random = new Random();
 
-		// bubbleSort(x);
-		// selectionSort(x);
-		// insertionSort(x);
-		// mergeSort(x);
-		// quickSort(x);
-		countingSort(x);
-		for (int i = 0; i < 5; i++)
-			System.out.println(x[i]);
+		// Cria o Array Ordenado de 20 elementos
+		for (int i = 0; i < 20; i++) {
+			arrayOrdenado[i] = i + 1;
+		}
+
+		// Cria o Array Ordenado Inversamente de 20 elementos
+		int j = 0;
+		for (int i = 19; i >= 0; i--, j++) {
+			arrayInversamenteOrd[i] = j + 1;
+		}
+		// Cria Array Aleatorio de 20 elementos 1 - 20
+		for (int i = 0; i < 20; i++) {
+			arrayAleatorio[i] = random.nextInt(20) + 1;
+		}
+
+		for (int i = 0; i < 20; i++) {
+
+			// System.out.println(arrayAleatorio[i]);
+
+		}
+
+		// Tempo Ordenado Bubble
+		long tempoBubbleOrd = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			bubbleSort(arrayOrdenado.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoBubbleOrd += tempo;
+
+		}
+		long mediaBubbleOrd = tempoBubbleOrd / 5;
+
+		System.out.println("Media Buble Ord: " + mediaBubbleOrd);
+		// Tempo Inversamente Ordenado Bubble
+		long tempoBubbleInv = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			bubbleSort(arrayInversamenteOrd.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoBubbleInv += tempo;
+
+		}
+		long mediaBubbleInv = tempoBubbleInv / 5;
+		System.out.println("Media Buble Inv: " + mediaBubbleInv);
+
+		// Tempo Aleatorio Bubble
+		long tempoBubbleAleatorio = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			bubbleSort(arrayAleatorio.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoBubbleAleatorio += tempo;
+
+		}
+		long mediaBubbleAleatoria = tempoBubbleAleatorio / 5;
+		System.out.println("Media Buble Aleatorio: " + mediaBubbleAleatoria);
+
+		// Tempo Ordenado Selection
+		long tempoSelectionOrd = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			selectionSort(arrayOrdenado.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoSelectionOrd += tempo;
+
+		}
+		long mediaSelectionOrd = tempoSelectionOrd / 5;
+
+		System.out.println("Media Selection Ord: " + mediaSelectionOrd);
+		// Tempo Inversamente Ordenado Selection
+		long tempoSelectionInv = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			selectionSort(arrayInversamenteOrd.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoSelectionInv += tempo;
+
+		}
+		long mediaSelectionInv = tempoSelectionInv / 5;
+
+		System.out.println("Media Selection Inv: " + mediaSelectionInv);
+
+		// Tempo Aleatorio Selection
+		long tempoSelectionAleatorio = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			selectionSort(arrayAleatorio.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoSelectionAleatorio += tempo;
+
+		}
+		long mediaSelectionAleatoria = tempoSelectionAleatorio / 5;
+
+		System.out.println("Media Selection Aleatorio: " + mediaSelectionAleatoria);
+
+		// Tempo Ordenado Insertion
+		long tempoInsertionOrd = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			insertionSort(arrayOrdenado.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoInsertionOrd += tempo;
+
+		}
+		long mediaInsertionOrd = tempoInsertionOrd / 5;
+
+		System.out.println("Media Insertion Ord: " + mediaInsertionOrd);
+		// Tempo Inversamente Ordenado Insertion
+		long tempoInsertionInv = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			insertionSort(arrayInversamenteOrd.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoInsertionInv += tempo;
+
+		}
+		long mediaInsertionInv = tempoInsertionInv / 5;
+		System.out.println("Media Insertion Inv: " + mediaInsertionInv);
+
+		// Tempo Aleatorio Insertion
+		long tempoInsertionAleatorio = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			insertionSort(arrayAleatorio.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoInsertionAleatorio += tempo;
+
+		}
+		long mediaInsertionAleatoria = tempoInsertionAleatorio / 5;
+		System.out.println("Media Insertion Aleatorio: " + mediaInsertionAleatoria);
+
+		// Tempo Ordenado Merge
+		long tempoMergeOrd = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			mergeSort(arrayOrdenado.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoMergeOrd += tempo;
+
+		}
+		long mediaMergeOrd = tempoMergeOrd / 5;
+
+		System.out.println("Media Merge Ord: " + mediaMergeOrd);
+		// Tempo Inversamente Ordenado Merge
+		long tempoMergeInv = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			mergeSort(arrayInversamenteOrd.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoMergeInv += tempo;
+
+		}
+		long mediaMergeInv = tempoMergeInv / 5;
+		System.out.println("Media Merge Inv: " + mediaMergeInv);
+
+		// Tempo Aleatorio Merge
+		long tempoMergeAleatorio = 0;
+
+		for (int i = 0; i < 5; i++) {
+			long antes = System.nanoTime();
+			mergeSort(arrayAleatorio.clone());
+			long depois = System.nanoTime();
+
+			long tempo = depois - antes;
+
+			tempoMergeAleatorio += tempo;
+
+		}
+		long mediaMergeAleatoria = tempoMergeAleatorio / 5;
+		System.out.println("Media Merge Aleatorio: " + mediaMergeAleatoria);
+		
+		//Tempo Ordenado Quick
+		long tempoQuickOrd = 0;
+		
+		for(int i = 0; i < 5; i++)
+		{ 
+			long antes = System.nanoTime();
+            quickSort(arrayOrdenado.clone());
+            long depois = System.nanoTime();
+          
+            long tempo = depois - antes;
+            
+           tempoQuickOrd += tempo;
+			
+		}
+		long mediaQuickOrd = tempoQuickOrd/5;
+		
+		System.out.println("Media Quick Ord: " +mediaQuickOrd);
+		//Tempo Inversamente Ordenado Quick
+				long tempoQuickInv = 0;
+				
+				for(int i = 0; i < 5; i++)
+				{ 
+					long antes = System.nanoTime();
+					quickSort(arrayInversamenteOrd.clone());
+		            long depois = System.nanoTime();
+		          
+		            long tempo = depois - antes;
+		            
+		           tempoQuickInv += tempo;
+					
+				}
+				long mediaQuickInv = tempoQuickInv/5;
+				System.out.println("Media QuickInv: " +mediaQuickInv);
+				
+				//Tempo Aleatorio Quick
+				long tempoQuickAleatorio= 0;
+				
+				for(int i = 0; i < 5; i++)
+				{ 
+					long antes = System.nanoTime();
+		            quickSort(arrayAleatorio.clone());
+		            long depois = System.nanoTime();
+		          
+		            long tempo = depois - antes;
+		            
+		           tempoQuickAleatorio += tempo;
+					
+				}
+				long mediaQuickAleatoria = tempoQuickAleatorio/5;
+				System.out.println("Media Quick Aleatorio: " +mediaQuickAleatoria);
+
+				//Tempo Ordenado Counting
+				long tempoCountingOrd = 0;
+				
+				for(int i = 0; i < 5; i++)
+				{ 
+					long antes = System.nanoTime();
+		            countingSort(arrayOrdenado.clone());
+		            long depois = System.nanoTime();
+		          
+		            long tempo = depois - antes;
+		            
+		           tempoCountingOrd += tempo;
+					
+				}
+				long mediaCountingOrd = tempoCountingOrd/5;
+				
+				System.out.println("Media Counting Ord: " +mediaCountingOrd);
+				//Tempo Inversamente Ordenado Bubble
+						long tempoCountingInv = 0;
+						
+						for(int i = 0; i < 5; i++)
+						{ 
+							long antes = System.nanoTime();
+				            countingSort(arrayInversamenteOrd.clone());
+				            long depois = System.nanoTime();
+				          
+				            long tempo = depois - antes;
+				            
+				           tempoCountingInv += tempo;
+							
+						}
+						long mediaCountingInv = tempoCountingInv/5;
+						System.out.println("Media Counting Inv: " +mediaCountingInv);
+						
+						//Tempo Aleatorio Bubble
+						long tempoCountingAleatorio= 0;
+						
+						for(int i = 0; i < 5; i++)
+						{ 
+							long antes = System.nanoTime();
+							countingSort(arrayAleatorio.clone());
+				            long depois = System.nanoTime();
+				          
+				            long tempo = depois - antes;
+				            
+				           tempoCountingAleatorio += tempo;
+							
+						}
+						long mediaCountingAleatoria = tempoCountingAleatorio/5;
+						System.out.println("Media Counting Aleatorio: " +mediaCountingAleatoria);
+
+
+
+		
 
 	}
 
@@ -179,24 +492,21 @@ public class BrincandoComOrdenacao {
 		}
 		int aux[] = new int[vetor[k] + 1];
 		int b[] = new int[vetor.length];
-		
-		for(int i = 0; i< aux.length; i++)
-			aux[i] = 0;
-		
-		for(int i = 0; i< vetor.length; i++)
-			aux[vetor[i]]+=1;
-		for(int i = 1; i <aux.length; i++)
-			aux[i] += aux[i-1];
-		
-		for(int i = 0; i < vetor.length; i++)
-			b[aux[vetor[i]]-- -1] = vetor[i];
-		
-		for(int i = 0; i < vetor.length; i++)
-			vetor[i] = b[i];
-	}	
-	
 
-	
+		for (int i = 0; i < aux.length; i++)
+			aux[i] = 0;
+
+		for (int i = 0; i < vetor.length; i++)
+			aux[vetor[i]] += 1;
+		for (int i = 1; i < aux.length; i++)
+			aux[i] += aux[i - 1];
+
+		for (int i = 0; i < vetor.length; i++)
+			b[aux[vetor[i]]-- - 1] = vetor[i];
+
+		for (int i = 0; i < vetor.length; i++)
+			vetor[i] = b[i];
+	}
 
 	private static void swap(int[] vetor, int i, int j) {
 
